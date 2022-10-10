3b2da76518844cfefcac9ddd1952571e8383e6e3
Facundoblanco10 <5078@holbertonstudents.com>
2022-10-10 16:41:43 -0300

    task3

---
+++ models/base_model.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+from uuid import uuid4
+from datetime import datetime
+
+class BaseModel:
+
+    def __init__(self, id=None):
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        st = f'[BaseModel] ({self.id} {self.__dict__})'
+        return (st)
+    
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        new_dict = {}
+        new_dict["id"] = self.id
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+        new_dict["__class__"] = f"{self.__class__.__name__}"
+        return (new_dict)
