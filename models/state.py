230aad91520130f9fc3e7fa2583a7e4fb1f70fdf
RicardoDanta <uruguaydenos@gmail.com>
2022-10-11 16:39:49 -0700

    State

--- models/state.py
+++ models/state.py
@@ -6,4 +6,6 @@ from models.base_models import BaseModel
 
 class State(BaseModel):
     """Define a Class"""
-    name = ""
+    def __init__(self, name):
+        """Name"""
+        self.name = ""
