199f99f299e3471bc8efde430f4b2f0357ffcb07
Facundoblanco10 <5078@holbertonstudents.com>
2022-10-11 14:51:48 -0300

    task5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,18 +10,18 @@ class FileStorage():
         return self.__objects
 
     def new(self, obj):
-        FileStorage.__objects[f"{__class__.__name__}.id"] = obj
+        self.__objects[f"{__class__.__name__}.id"] = obj
 
     def save(self):
         try:
-            with open(FileStorage.__file_path, "w") as f:
-                f.write(json.dumps(FileStorage.__objects))
+            with open(self.__file_path, "w") as f:
+                f.write(json.dumps(self.__objects))
         except:
             pass
 
     def reload(self):
         try:
-            with open(FileStorage.__file_path, "r") as f:
+            with open(self.__file_path, "r") as f:
                 self.__objects = json.loads(f)
         except:
             pass
