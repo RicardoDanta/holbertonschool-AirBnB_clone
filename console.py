63cecc75e67ba6b7912543ab1e7fcdb7e520d31d
RicardoDanta <uruguaydenos@gmail.com>
2022-10-11 14:01:41 -0700

    Fixing Classes

--- console.py
+++ console.py
@@ -4,19 +4,15 @@ import cmd
 import sys
 from models import FileStorage
 from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
+
 
 class HBNBCommand(cmd.Cmd):
     """HBNB"""
     prompt = "(hbnb)"
 
-    def do_create(self, args):
-        """Create"""
+    def EmptyLine(self):
+        """Print Anything"""
+        pass
 
     def do_help(self, args):
         """Help"""
@@ -30,9 +26,6 @@ class HBNBCommand(cmd.Cmd):
         """Quit"""
         return True
 
-    def EmptyLine(self):
-        """Print Anything"""
-        pass
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
