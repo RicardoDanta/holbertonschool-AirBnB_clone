b5de87cb82a302854653d3f0233d1e7d7e8df983
Facundoblanco10 <5078@holbertonstudents.com>
2022-10-11 19:35:18 -0300

    task5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -13,7 +13,7 @@ class FileStorage():
         return self.__objects
 
     def new(self, obj):
-        self.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj.__dict__
+        self.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
 
     def save(self):
         with open(self.__file_path, "w") as f:
@@ -23,5 +23,6 @@ class FileStorage():
         try:
             with open(self.__file_path, "r") as f:
                 self.__objects = json.load(f)
+                self.__objects = BaseModel(self.__objects)
         except:
             pass
