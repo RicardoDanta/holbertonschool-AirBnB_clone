2ec91bfd61a9a61d2750ffcb0534c4d9bc3d03cf
Facundoblanco10 <5078@holbertonstudents.com>
2022-10-13 12:17:52 -0300

    comments

--- models/base_model.py
+++ models/base_model.py
@@ -5,8 +5,15 @@ import models
 
 
 class BaseModel():
+    """
+    Defines all common attributes/methods for other classes
+    """
 
     def __init__(self, *args, **kwargs):
+        """
+        This function is the constructor for the BaseModel class. It creates a new instance of the
+        BaseModel class and assigns it an id, created_at, and updated_at
+        """
         format = "%Y-%m-%dT%H:%M:%S.%f"
         if kwargs:
             for key in kwargs:
@@ -23,13 +30,23 @@ class BaseModel():
         models.storage.new(self)
 
     def __str__(self):
+        """
+        The function returns a string representation of the object
+        """
         return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
 
     def save(self):
+        """
+        The save function updates the updated_at attribute of the object to the current time and saves
+        the object to the JSON file.
+        """
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
+        """
+        This function takes in an object and returns a dictionary representation of the object
+        """
         new_dict = self.__dict__.copy()
         new_dict["created_at"] = self.created_at.isoformat("T")
         new_dict["updated_at"] = self.updated_at.isoformat("T")
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -14,16 +14,26 @@ import json
 
 
 class FileStorage():
+    """
+    Serializes instances to a JSON file and deserializes JSON file to instances
+    """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
+        """
+        It returns all the objects in the class
+        """
         return self.__objects
 
     def new(self, obj):
         self.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
 
     def save(self):
+        """
+        Takes in an instance of FileStorage and writes the JSON string
+        representation of the dictionary __objects to the file __file_path
+        """
         new_dict = {}
         for key in self.__objects.keys():
             new_dict[key] = self.__objects[key].to_dict()
@@ -31,6 +41,9 @@ class FileStorage():
             f.write(json.dumps(new_dict))
 
     def reload(self):
+        """
+        It opens the file, reads it, and then converts the JSON string into a Python object
+        """
         if os.path.exists(self.__file_path):
             with open(self.__file_path, "r") as f:
                 aux = json.loads(f.read())
