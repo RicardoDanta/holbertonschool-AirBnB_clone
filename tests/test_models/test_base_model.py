db9d133c92a8e98d7304a52cc2fb6984cf2523f3
RicardoDanta <uruguaydenos@gmail.com>
2022-10-14 15:21:36 -0700

    Test BaseModel

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,23 +1,14 @@
 #!/usr/bin/python3
-"""Unittests for BaseModel"""
-
-import unittest
 from models.base_model import BaseModel
 
-
-class TestBaseModel(unittest.TestCase):
-    """BaseModel"""
-
-    def test_save(self):
-        basemodel = BaseModel()
-        basemodel.save()
-        self.assertNotEqual(basemodel.created_at, basemodel.updated_at)
-
-    def test_to_dict(self):
-        basemodel = BaseModel()
-        basemodel.to_dict()
-        self.assertNotEqual(basemodel.created_at, basemodel.update_at, self.__class__)
-
-
-if __name__ == '__main__':
-    unittest.main()
+my_model = BaseModel()
+my_model.name = "My First Model"
+my_model.my_number = 89
+print(my_model)
+my_model.save()
+print(my_model)
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
