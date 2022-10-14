5c44ce4b2c9c7a19c7e310c75ed322c3637ccdbe
RicardoDanta <uruguaydenos@gmail.com>
2022-10-14 13:30:41 -0700

    Test Base Model

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Unittests for BaseModel"""
+
+import unittest
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    """BaseModel"""
+
+    def test_save(self):
+        basemodel = BaseModel()
+        basemodel.save()
+        self.assertNotEqual(basemodel.created_at, basemodel.updated_at)
+
+    def test_to_dict(self):
+        basemodel = BaseModel()
+        basemodel.to_dict()
+        self.assertNotEqual(basemodel.created_at, basemodel.updated_at, basemodel.__class__)
+
+
+if __name__ == '__main__':
+    unittest.main()
