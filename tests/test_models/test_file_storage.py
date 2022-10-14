168082a96cb65c20c8be1aca6d3f7f500e3c534e
RicardoDanta <uruguaydenos@gmail.com>
2022-10-14 14:16:11 -0700

    Test FileStorage

---
+++ tests/test_models/test_file_storage.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Test FileStorage"""
+
+import unittest
+from models.engine import FileStorage
+
+
+class TestFileStorage(unittest.TestCase):
+    """Testing"""
+
+    def test_all(self):
+        storage = FileStorage()
+        self.assertEqual(storage.self.__objects)
+
+
+if __name__ == '__main__':
+    unittest.main()
