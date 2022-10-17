902336fa95e54ae6b26d4e6ecd81740e9de6ffac
RicardoDanta <uruguaydenos@gmail.com>
2022-10-17 05:06:29 -0700

    Removing str from BaseModel Test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -15,7 +15,7 @@ class TestBaseModel(unittest.TestCase):
         basemodel.save()
         self.assertNotEqual(basemodel.created_at, basemodel.updated_at)
 
-    def test_basemodel_to_dict(self):
+    def test__to_dict(self):
         """Check BaseModel to_dict"""
         basemodel = BaseModel()
         basemodel_dict = basemodel.to_dict()
@@ -27,13 +27,6 @@ class TestBaseModel(unittest.TestCase):
         self.assertIn("created_at", basemodel.to_dict())
         self.assertIn("updated_at", basemodel.to_dict())
         self.assertIn("__class__", basemodel.to_dict())
-
-    def test__str__(self):
-        """Checks __str__"""
-        basemodel = BaseModel()
-        self.assertEqual(
-            f"[{type(basemodel).__name__}] \
-                ({basemodel.id}) {basemodel.__dict__}", str(basemodel))
 
 
 if __name__ == '__main__':
